b8f3f610dcbb06f0da0f40667b21aada0e08c7e2
Simon Praetorius <praetorius@sitegeist.de>
2023-01-19 10:33:27 +0100

    [BUGFIX] Check for height and width individually

--- Classes/ViewHelpers/Image/Modify/ScaleViewHelper.php
+++ Classes/ViewHelpers/Image/Modify/ScaleViewHelper.php
@@ -16,7 +16,7 @@ class ScaleViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelp
         $this->registerArgument('imageSource', ImageSource::class, 'Image source (if not provided via content)');
         $this->registerArgument('height', 'integer', 'Desired image height');
         $this->registerArgument('width', 'integer', 'Desired image width');
-        $this->registerArgument('maxDimensions', 'boolean', 'If true, height and with will be considered maximums', true, false);
+        $this->registerArgument('maxDimensions', 'boolean', 'If true, height and width will be considered maximums', true, false);
     }
 
     public static function renderStatic(
@@ -27,8 +27,8 @@ class ScaleViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelp
         $imageSource = $arguments['imageSource'] ?? $renderChildrenClosure();
 
         if ($arguments['height'] || $arguments['width']) {
-            $heightFactor = $arguments['height'] / $imageSource->getOriginalImage()->getHeight();
-            $widthFactor = $arguments['width'] / $imageSource->getOriginalImage()->getWidth();
+            $heightFactor = $arguments['height'] ? $arguments['height'] / $imageSource->getOriginalImage()->getHeight() : 1;
+            $widthFactor = $arguments['width'] ? $arguments['width'] / $imageSource->getOriginalImage()->getWidth() : 1;
             $scaleFactor = ($arguments['maxDimensions'])
                 ? min($heightFactor, $widthFactor)
                 : max($heightFactor, $widthFactor);
